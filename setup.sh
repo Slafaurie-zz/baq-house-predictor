90ba7e18a1ff983e0d849f36954d577ac8544a4b
slafaurie <selafaurie@gmail.com>
2021-01-25 08:18:05 -0500

    modify procfile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
-# mkdir -p ~/.streamlit/
-# echo "\
-# [general]\n\
-# email = \"selafaurie@gmail.com\"\n\
-# " > ~/.streamlit/credentials.toml
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"selafaurie@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless=true\n\
+enableCORS=false\n\
+port=$PORT\n\
+" > ~/.streamlit/config.toml
